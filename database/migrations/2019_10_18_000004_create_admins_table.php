5352e113abe93572eae9e3c78a4437323fa08e3a
Rafael Gomes <faelfg@gmail.com>
2019-10-17 23:49:42 -0300

    Initial commit

---
+++ database/migrations/2019_10_18_000004_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 80);
+            $table->string('email', 60)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 190);
+            $table->integer('profile_id')->unsigned();
+            $table->foreign('profile_id')->on('profiles')->references('id');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
---
+++ database/migrations/2019_10_18_000005_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 70);
+            $table->decimal('price', 10);
+            $table->string('description', 180);
+            $table->char('is_approved', 1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
